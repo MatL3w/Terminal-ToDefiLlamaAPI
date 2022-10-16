46ddd030b8414a5f179b1bb5ea67d4a18170ecf7
Lecislav <lecislav@protonmail.com>
2022-10-16 19:12:26 +0200

    complete explorer for protocols and chains

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-//modules 
+//modules
 const readline = require("node:readline");
 const fs = require("node:fs");
 const https = require("node:https");
 
 //variables
 let rl;
-let inputData='';
+let inputData = "";
+
 let index;
-const search=[
-    {name:'protocols', function:protocolsInformation},
-    {name:'chains', function:protocolsInformation}];
+let strData = "";
+let strDataParsed;
+let nextData;
+const search = [
+  { name: "protocols", function: getInformation },
+  { name: "chains", function: getInformation },
+];
 
 //CORE
 introduction();
@@ -18,54 +23,104 @@ readingFromConsole(analyzeData);
 
 //Functions
 function readingFromConsole(analyze) {
-    rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-      prompt: "#",
-    });
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "#",
+  });
+  rl.prompt();
+
+  rl.on("line", (line) => {
     rl.prompt();
-   
-    rl.on("line", (line) => {
-      rl.prompt();
-      inputData=line.trim();
-        switch (inputData) {
-        case "close":
-          console.log("closing input stream");
-          rl.exit();
-          break;
-        default:
-            analyze();
-          rl.prompt();
-          break;
-      }
-    });
-    // add event listner for closing streams and also process
-    rl.on("close", () => {
-      console.log("CLOSED");
-      process.exit(0);
-    });
-  }
-function introduction(){
-    console.log("Scripts explore data about Cryptocurrency from DefiLlama.");
-    console.log("Input Type of data to explore");
-  }
-function analyzeData(){
-   if(!index){
-    for(let i=0;i<search.length;i++){
-        if(inputData===search[i].name){
-        index=i;
-        search[index].function();
-        console.log('lol');
-        };
-    }}
-    else{
-        search[index].function();
+    inputData = line.trim();
+    switch (inputData) {
+      case "close":
+        console.log("closing input stream");
+        rl.close();
+        break;
+        case "reset":
+            console.log("reset");
+            console.clear();
+            reset();
+            break;
+      default:
+        analyze();
+        rl.prompt();
+        break;
     }
-}   
-function protocolsInformation(){
-
-
+  });
+  // add event listner for closing streams and also process
+  rl.on("close", () => {
+    console.log("CLOSED");
+    process.exit(0);
+  });
+}
+function introduction() {
+  console.log("Scripts explore data about Cryptocurrency from DefiLlama.");
+  console.log("Input Type of data to explore");
+}
+function analyzeData() {
+  if (index===undefined) {
+    for (let i = 0; i < search.length; i++) {
+      if (inputData === search[i].name) {
+        index = i;
+        search[index].function(saveDataToFile);
+        console.log("lol");
+      }
+    }
+  } else {
+    search[index].function(saveDataToFile);
   }
-function httpsGetFuntion(typeOfData){
-    
+}
+function getInformation(saveFunction) {
+    if(!strData){
+        httpsGetRequest(search[index].name, saveDataToFile);
+        console.log(`Type next value`)
+    }
+    else{
+        nextData=inputData;
+        for(let i=0;i<strDataParsed.length;i++){
+            if(strDataParsed[i].name===nextData){
+                console.log(i);
+                console.log(strDataParsed[i]);
+                saveFunction(search[index].name+nextData,JSON.stringify(strDataParsed[i]));
+
+               
+            }
+        }
+        console.log(`Type correct value for: ${search[index].name}`);
+    }
+}
+function httpsGetRequest(information, saveFunction) {
+  const options = {
+    hostname: "api.llama.fi",
+    port: 443,
+    path: `/${information}`,
+    method: "GET",
+  };
+  https
+    .request(options, (res) => {
+      res.on("data", (chunk) => {
+        strData += chunk;
+      });
+      res.on("end", () => {
+        strDataParsed=JSON.parse(strData);
+        saveFunction(information, strData);
+      });
+    })
+    .end();
+
+  //
+}
+function saveDataToFile(filename, content) {
+  fs.writeFile(filename, content, (err) => {
+    if (err) throw err;
+  });
+}
+function reset(){
+    index=undefined;
+    strData = '';
+    strDataParsed=undefined;
+    nextData='';
+    introduction();
 }
